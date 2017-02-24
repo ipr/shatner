4197aabac7f9b74177ec3da00a269c43aa822929
Ilkka Prusi <ilkka.prusi@gmail.com>
2017-02-24 11:11:58 +0200

    initial commit
    first commit

---
+++ shatner.cpp
@@ -0,0 +1,23 @@
+// shatner.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "shatner.h"
+
+#include "MemoryMappedFile.h"
+
+
+// Global Variables:
+HINSTANCE g_hInst;                                // current instance
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	g_hInst = hInstance;
+
+
+	return 0;
+}
+
